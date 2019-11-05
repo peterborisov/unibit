d932c522a1de6412bd96c2ae8e4fd097fb451a63
peterborisov <borisov.peter@yahoo.co.uk>
2019-11-05 20:20:43 +0200

    additional tasks

---
+++ Първи курс/Основи на програмирането/Programming-basic-homework-6-nov-20219/7.nFactorial.cs
@@ -0,0 +1,27 @@
+﻿
+//7. Да не изчисли N! (1 ≤ N ≤ 12), като се има предвид че 5!=5*4*3*2*1 или 9!= 9*8*7*6*5*4*3*2*1. 
+//Нека от конзолата да се зададе стойност за N.
+//На екрана да се отпечата следното:
+//„ N! = ………………“ , където N да е въведеното от потребителя число, а след равенството да се извежда стойността на N!
+
+using System;
+
+namespace nFactorial
+{
+    class nFactorial
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
---
+++ Първи курс/Основи на програмирането/Programming-basic-homework-6-nov-20219/9.sortArray.cs
@@ -0,0 +1,34 @@
+﻿//9. Да се въведе от конзолата едномерен масив от цели числа и да се сортира по низходящ ред.
+//Да се изведе сортирания масив.
+//Да се изведе минималния му елемент.
+
+using System;
+using System.Linq;
+
+namespace sortArray
+{
+    class sortArray
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToString();
+            int[] ia = input.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            // Sort array in ascending order. 
+            Array.Sort(ia);
+
+            // reverse array 
+            Array.Reverse(ia);
+
+            // print all element of array 
+            Console.WriteLine("In descending order:");
+            foreach (int value in ia)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Min value is: {0}", ia.Last());
+
+        }
+    }
+}
+
